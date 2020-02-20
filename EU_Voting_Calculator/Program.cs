2d4f447d4bcbdc3ab8a13d29a35a044c294e5129
40017465 <61196198+40017465@users.noreply.github.com>
2020-02-20 15:50:15 +0000

    Add files via upload

---
+++ EU_Voting_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EU_Voting_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
