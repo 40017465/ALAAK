efce4d56c1ba96545f74fad8eb1a554015047239
Alex Linder <40017465@edu.ucnl.ac.uk>
2020-02-27 15:24:17 +0000

    Added the EuroZone class.

--- Please everyone use this file for the assignment/EU_Calculator/Voting_Rules.cs
+++ Please everyone use this file for the assignment/EU_Calculator/Voting_Rules.cs
@@ -62,5 +62,20 @@ namespace EU_Calculator
 
             }
         }
+
     }
+    public static int EuroZone()
+        {
+            Console.WriteLine("Would you like to incluce non Euro Zone? 1:Yes 2:No");
+            int Answer = int.Parse(Console.ReadLine());
+            if (Asnwer == 1)
+            {
+                Country.GetCountry();
+            }
+
+            else 
+            {
+                Country.RemoveCountry();            
+            }              
+        }
 }
